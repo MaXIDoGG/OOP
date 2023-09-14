2ae64d7e04fd112083beb11cf8fe7cf6cec42d00
Nasonov <Nasonov.2021@stud.nstu.ru>
2023-09-14 11:49:48 +0300

    delete main

--- lab1/main.cpp
+++
@@ -1,9 +0,0 @@
-#include "pirsonII.hpp"
-
-using namespace std;
-
-int main() {
-    double x = 0, v = 7, u = 1, lam = 4;
-    cout << v << "\n" << dispersion(v, lam) << "\n" << exces(v) << "\n" << density(x, v, u, lam);
-    return 0;
-}
