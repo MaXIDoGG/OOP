9bb892857f75b4f2ceafcea91aa85ce89c9bc52e
Arina <arina.safronova@inbox.ru>
2023-09-15 22:39:42 +0700

    Fixed functions

--- lab1/mix.cpp
+++ lab1/mix.cpp
@@ -13,62 +13,59 @@ double densityMix(double x, double p, double v1, double u1, double lam1, double 
 }
 
 //Ф-ия мат. ожидания
-double mathexpMix(double p, double a, double b, double v1, double u1, double lam1, double v2, double u2, double lam2) {
-    double mathExp1 = mathexp(a, b, v1, u1, lam1);
-    double mathExp2 = mathexp(a, b, v2, u2, lam2);
+double mathexpMix(double p, double v1, double u1, double lam1, double v2, double u2, double lam2) {
+    double mathExp1 = mathexp(-1, 1, v1, u1, lam1);
+    double mathExp2 = mathexp(-1, 1, v2, u2, lam2);
     double result = (1 - p) * mathExp1 + p * mathExp2;
     return result;
 }
 
 //Ф-ия Дисперсии
-//передаю a и b чтобы посчитать мат. ожидание, но наверное они всё-таки по умолчанию -1 1 
-double dispersionMix(double p, double a, double b, double v1, double u1, double lam1, double v2, double u2, double lam2) {
-    double mathExpMix = mathexpMix(p, a, b, v1, u1, lam1, v2, u2, lam2);
-    double mathExp1 = mathexp(a, b, v1, u1, lam1);
-    double mathExp2 = mathexp(a, b, v2, u2, lam2);
+double dispersionMix(double p, double v1, double u1, double lam1, double v2, double u2, double lam2) {
+    double mathExpMix = mathexpMix(p, v1, u1, lam1, v2, u2, lam2);
+    double mathExp1 = mathexp(-1, 1, v1, u1, lam1);
+    double mathExp2 = mathexp(-1, 1, v2, u2, lam2);
     double dispersion1 = dispersion(v1, lam1);
     double dispersion2 = dispersion(v2, lam2);
-    double result = (1-p) * (mathExp1 * mathExp1 + dispersion1) + p * (mathExp2 * mathExp2 + dispersion2) - mathExpMix * mathExpMix; 
+    double result = (1 - p) * (pow(mathExp1, 2) + dispersion1) + p * (pow(mathExp2, 2) + dispersion2) - pow(mathExpMix, 2); 
     return result;
 }
 
 //Коэф. асимметрии
-double asymmetryMix(double p, double a, double b, double v1, double u1, double lam1, double v2, double u2, double lam2) {
-
-    double mathExpMix = mathexpMix(p, a, b, v1, u1, lam1, v2, u2, lam2);
-    double mathExp1 = mathexp(a, b, v1, u1, lam1);
-    double mathExp2 = mathexp(a, b, v2, u2, lam2);
+double asymmetryMix(double p, double v1, double u1, double lam1, double v2, double u2, double lam2) {
+    double mathExpMix = mathexpMix(p, v1, u1, lam1, v2, u2, lam2);
+    double mathExp1 = mathexp(-1, 1, v1, u1, lam1);
+    double mathExp2 = mathexp(-1, 1, v2, u2, lam2);
     double dispersion1 = dispersion(v1, lam1);
     double dispersion2 = dispersion(v2, lam2);
-    double dispersionMixx = dispersionMix(p, a, b, v1, u1, lam1, v2, u2, lam2);
+    double dispersionMixx = dispersionMix(p, v1, u1, lam1, v2, u2, lam2);
     double assymetry1 = asymmetry(v1, u1, lam1);
     double assymetry2 = asymmetry(v2, u2, lam2);
     double result = 1 / pow(dispersionMixx, 3 / 2) * (
-        (1-p) * (pow(mathExp1 - mathExpMix, 3) + 3 * (mathExp1 - mathExpMix) * dispersion1 + pow(dispersion1, 3/2)*assymetry1) + 
+        (1 - p) * (pow(mathExp1 - mathExpMix, 3) + 3 * (mathExp1 - mathExpMix) * dispersion1 + pow(dispersion1, 3/2)*assymetry1) + 
         p * (pow(mathExp2 - mathExpMix, 3) + 3 * (mathExp2 - mathExpMix) * dispersion2 + pow(dispersion2, 3 / 2) * assymetry2)
         );
     return result;
 }
 
 //Ф-ия эксцесса
-double excesMix(double p, double a, double b, double v1, double u1, double lam1, double v2, double u2, double lam2) {
-    double mathExpMix = mathexpMix(p, a, b, v1, u1, lam1, v2, u2, lam2);
-    double mathExp1 = mathexp(a, b, v1, u1, lam1);
-    double mathExp2 = mathexp(a, b, v2, u2, lam2);
+double excesMix(double p, double v1, double u1, double lam1, double v2, double u2, double lam2) {
+    double mathExpMix = mathexpMix(p, v1, u1, lam1, v2, u2, lam2);
+    double mathExp1 = mathexp(-1, 1, v1, u1, lam1);
+    double mathExp2 = mathexp(-1, 1, v2, u2, lam2);
     double dispersion1 = dispersion(v1, lam1);
     double dispersion2 = dispersion(v2, lam2);
-    double dispersionMixx = dispersionMix(p, a, b, v1, u1, lam1, v2, u2, lam2);
+    double dispersionMixx = dispersionMix(p, v1, u1, lam1, v2, u2, lam2);
     double assymetry1 = asymmetry(v1, u1, lam1);
     double assymetry2 = asymmetry(v2, u2, lam2);
     double exces1 = exces(v1);
     double exces2 = exces(v2);
-
     double result = 1 / pow(dispersionMixx, 2) * (
         (1 - p) * (pow(mathExp1 - mathExpMix, 4) + 6 * pow(mathExp1 - mathExpMix, 2) * dispersion1 + 4 * (mathExp1 - mathExpMix) * 
-            pow(dispersion1, 3/2)*assymetry1 + pow(dispersion1, 2) * exces1) + 
+            pow(dispersion1, 3 / 2) * assymetry1 + pow(dispersion1, 2) * exces1) + 
         p * (pow(mathExp2 - mathExpMix, 4) + 6 * pow(mathExp2 - mathExpMix, 2) * dispersion2 + 4 * (mathExp2 - mathExpMix) *
             pow(dispersion2, 3 / 2) * assymetry2 + pow(dispersion2, 2) * exces2)
-        );
+        ) - 3;
     return result;
 }
 
--- lab1/mix.hpp
+++ lab1/mix.hpp
@@ -4,8 +4,8 @@
 #include <math.h>
 #include <time.h>
 
-double densityMix(double u1, double v1, double lam1, double u2, double v2, double lam2, double x, double p); //+
-double mathexpMix(double p, double a, double b, double v1, double u1, double lam1, double v2, double u2, double lam2); //+
-double dispersionMix(double p, double a, double b, double v1, double u1, double lam1, double v2, double u2, double lam2); //+
-double asymmetryMix(double p, double a, double b, double v1, double u1, double lam1, double v2, double u2, double lam2); //+
-double excesMix(double p, double a, double b, double v1, double u1, double lam1, double v2, double u2, double lam2); //++
+double densityMix(double u1, double v1, double lam1, double u2, double v2, double lam2, double x, double p); 
+double mathexpMix(double p, double v1, double u1, double lam1, double v2, double u2, double lam2); 
+double dispersionMix(double p, double v1, double u1, double lam1, double v2, double u2, double lam2); 
+double asymmetryMix(double p, double v1, double u1, double lam1, double v2, double u2, double lam2); 
+double excesMix(double p, double v1, double u1, double lam1, double v2, double u2, double lam2); 
