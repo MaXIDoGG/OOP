4d7eeaee6eee5b1d944bd1fd90dfb2bb35d5eead
Arina <arina.safronova@inbox.ru>
2023-09-17 00:55:32 +0700

    Доработаны операции, добавлена операция моделирования случ. вел. отчасти написаны тесты

--- lab1/mix.cpp
+++ lab1/mix.cpp
@@ -1,4 +1,5 @@
 #include "pirsonII.hpp"
+#include <cassert>
 
 //v параметр формы он же ню
 //u параметр сдвига он же мю
@@ -62,13 +63,24 @@ double excesMix(double p, double v1, double u1, double lam1, double v2, double u
     double exces2 = exces(v2);
     double result = 1 / pow(dispersionMixx, 2) * (
         (1 - p) * (pow(mathExp1 - mathExpMix, 4) + 6 * pow(mathExp1 - mathExpMix, 2) * dispersion1 + 4 * (mathExp1 - mathExpMix) * 
-            pow(dispersion1, 3 / 2) * assymetry1 + pow(dispersion1, 2) * exces1) + 
+            pow(dispersion1, 3 / 2) * assymetry1 + pow(dispersion1, 2) * (exces1 + 3)) + 
         p * (pow(mathExp2 - mathExpMix, 4) + 6 * pow(mathExp2 - mathExpMix, 2) * dispersion2 + 4 * (mathExp2 - mathExpMix) *
-            pow(dispersion2, 3 / 2) * assymetry2 + pow(dispersion2, 2) * exces2)
+            pow(dispersion2, 3 / 2) * assymetry2 + pow(dispersion2, 2) * (exces2 + 3))
         ) - 3;
     return result;
 }
 
+void testMix() {
+    //x = 0, p = 0.5, u1 = u2 = 1, v1 = v2 = 0.5, lam1 = lam2 = 2
+    assert(fabs(densityMix(0, 0.5, 0.5, 1, 2, 0.5, 1, 2) - 0.275668) < 0.001);
+    assert(fabs(mathexpMix(0.5, 0.5, 1, 2, 0.5, 1, 2) - 0.075588 < 0.001));
+    assert(fabs(dispersionMix(0.5, 0.5, 1, 2, 0.5, 1, 2) - 1) < 0.001);
+    // не вышло посчитать коэфф ассиметрии и соответственно эксцесс тоже
+    //std::cout << asymmetry(0.5, 1, 2) << std::endl;
+    //std::cout << asymmetryMix(0.5, 0.5, 1, 2, 0.5, 1, 2) << std::endl;
 
+
+    std::cout << "All tests done" << std::endl;
+}
 
 
--- lab1/mix.hpp
+++ lab1/mix.hpp
@@ -4,8 +4,9 @@
 #include <math.h>
 #include <time.h>
 
-double densityMix(double u1, double v1, double lam1, double u2, double v2, double lam2, double x, double p); 
-double mathexpMix(double p, double v1, double u1, double lam1, double v2, double u2, double lam2); 
-double dispersionMix(double p, double v1, double u1, double lam1, double v2, double u2, double lam2); 
-double asymmetryMix(double p, double v1, double u1, double lam1, double v2, double u2, double lam2); 
-double excesMix(double p, double v1, double u1, double lam1, double v2, double u2, double lam2); 
+double densityMix(double x, double p, double v1, double u1, double lam1, double v2, double u2, double lam2);
+double mathexpMix(double p, double v1, double u1, double lam1, double v2, double u2, double lam2);
+double dispersionMix(double p, double v1, double u1, double lam1, double v2, double u2, double lam2);
+double asymmetryMix(double p, double v1, double u1, double lam1, double v2, double u2, double lam2);
+double excesMix(double p, double v1, double u1, double lam1, double v2, double u2, double lam2);
+void testMix();
